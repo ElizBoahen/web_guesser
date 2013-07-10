4b1043ec9ad8915d96dc6f210478801654ae9e0b
Elizabeth A. Boahen <elizboahen@yahoo.com>
2013-07-10 11:48:14 -0400

    Fixing up the 'Youre guess is' errors nd fixing responses

--- Web_Guesser.rb
+++ Web_Guesser.rb
@@ -4,12 +4,20 @@ require 'sinatra/reloader'
 number = (1 + rand(99)).to_s
 
 get "/" do 
-	if params["guess"].to_i > number.to_i
-		guess = "Too High!"
+	if params["guess"].to_i == 0
+		guess_string = "That's not a number. Please guess a number between 1 and 100"
+	elsif params["guess"] == nil
+		guess_empty = " " 	
+	elsif params["guess"].to_i > number.to_i + 10
+		guess = "You're guess is WAY too high!!"
+	elsif params["guess"].to_i > number.to_i
+		guess = "You're guess is too high."	
+	elsif params["guess"].to_i < number.to_i + 10
+		guess = "You're guess is WAY too low!!"	
 	elsif params["guess"].to_i < number.to_i
-		guess = "Too Low"
+		guess = "You're guess is too low."
 	else
-		guess = "Correct"
+		guess = "You are Correct"
 	end
 	erb :index, :locals => {:number => number, :guess => guess}
 end
