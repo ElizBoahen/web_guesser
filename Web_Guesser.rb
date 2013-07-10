e706c8c491df255f2fc3af20c12e052e5370ee51
Elizabeth A. Boahen <elizboahen@yahoo.com>
2013-07-10 12:12:22 -0400

    Fixing the empty box error and variable error

--- Web_Guesser.rb
+++ Web_Guesser.rb
@@ -3,9 +3,9 @@ require 'sinatra/reloader'
 
 number = (1 + rand(99)).to_s
 
-get "/" do 
+get "/" do
 	if params["guess"].to_i == 0
-		guess_string = "That's not a number. Please guess a number between 1 and 100"
+		guess_string = "Please guess a number between 1 and 100"
 	elsif params["guess"] == nil
 		guess_empty = " " 	
 	elsif params["guess"].to_i > number.to_i + 10
@@ -19,5 +19,5 @@ get "/" do
 	else
 		guess = "You are Correct"
 	end
-	erb :index, :locals => {:number => number, :guess => guess}
+	erb :index, :locals => {:number => number, :guess => guess, :guess_empty => guess_empty, :guess_string => guess_string}
 end
